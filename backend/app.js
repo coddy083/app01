e75bb3fd1bf2cdd834c788ab494240c2a0315458
coddy083 <coddy083@gmail.com>
2023-04-12 15:44:35 +0900

    fix

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,21 @@ const cors = require("cors");
 const user = require("./route/user");
 const port = 9000;
 
+const allowedOrigins = ["http://localhost:3000", "http://49.247.47.141:4000"];
+
 app.use(
   cors({
-    origins: ["http://localhost:3000", "http://49.247.47.141:4000"],
+    origin: (origin, callback) => {
+      // 모든 요청을 허용하려면 아래와 같이 설정합니다.
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
   })
 );
 
