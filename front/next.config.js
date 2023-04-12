012cc586eee948b4a9dcd9b9dfca4e1db2edf9e9
coddy083 <coddy083@gmail.com>
2023-04-12 15:32:09 +0900

    fix

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.alias["~"] = __dirname;
+    return config;
+  },
+  pageExtensions: ["tsx"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
